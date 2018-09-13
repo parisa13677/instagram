63a8ce717841a2144f604ade88006fef9a20c8e6
parisa <parisa.golanbary@gmail.com>
2018-09-13 17:03:04 +0430

    Add devise and association with Pics.

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
+# this is Pic Model
 class Pic < ApplicationRecord
+  belongs_to :user
 end
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# this is User Model
+class User < ApplicationRecord
+  has_many :pics
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
